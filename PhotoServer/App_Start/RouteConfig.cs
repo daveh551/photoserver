3d3ee765788e8fbc73c69307e83dabb57767609d
Dave Hanna <dave@infotouchsys.com>
2013-03-18 20:50:15 -0500

    Add Elmah configuration

--- PhotoServer/App_Start/RouteConfig.cs
+++ PhotoServer/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace PhotoServer
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+	        routes.IgnoreRoute("elmah.axd");
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(

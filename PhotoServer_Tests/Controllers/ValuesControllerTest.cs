b1afb10a17472f1b5d72a5370f193004ca372c09
Dave Hanna <dave@infotouchsys.com>
2013-03-20 20:28:32 -0500

    Comment out ValuesController Tests for right now

--- PhotoServer_Tests/Controllers/ValuesControllerTest.cs
+++ PhotoServer_Tests/Controllers/ValuesControllerTest.cs
@@ -13,69 +13,69 @@ namespace PhotoServer.Tests.Controllers
     [TestFixture]
     public class ValuesControllerTest
     {
-        [Test]
-        public void Get()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+		//[Test]
+		//public void Get()
+		//{
+		//	// Arrange
+		//	ValuesController controller = new ValuesController();
 
-            // Act
-            IEnumerable<string> result = controller.Get();
+		//	// Act
+		//	IEnumerable<string> result = controller.Get();
 
-            // Assert
-            Assert.That(result, Is.Not.Null);
-            Assert.That(2, Is.EqualTo(result.Count()));
-            Assert.That("value1", Is.EqualTo(result.ElementAt(0)));
-            Assert.That("value2", Is.EqualTo(result.ElementAt(1)));
-        }
+		//	// Assert
+		//	Assert.That(result, Is.Not.Null);
+		//	Assert.That(2, Is.EqualTo(result.Count()));
+		//	Assert.That("value1", Is.EqualTo(result.ElementAt(0)));
+		//	Assert.That("value2", Is.EqualTo(result.ElementAt(1)));
+		//}
 
-        [Test]
-        public void GetById()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+		//[Test]
+		//public void GetById()
+		//{
+		//	// Arrange
+		//	ValuesController controller = new ValuesController();
 
-            // Act
-            string result = controller.Get(5);
+		//	// Act
+		//	string result = controller.Get(5);
 
-            // Assert
-            Assert.That("value", Is.EqualTo(result));
-        }
+		//	// Assert
+		//	Assert.That("value", Is.EqualTo(result));
+		//}
 
-        [Test]
-        public void Post()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+		//[Test]
+		//public void Post()
+		//{
+		//	// Arrange
+		//	ValuesController controller = new ValuesController();
 
-            // Act
-            controller.Post("value");
+		//	// Act
+		//	controller.Post("value");
 
-            // Assert
-        }
+		//	// Assert
+		//}
 
-        [Test]
-        public void Put()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+		//[Test]
+		//public void Put()
+		//{
+		//	// Arrange
+		//	ValuesController controller = new ValuesController();
 
-            // Act
-            controller.Put(5, "value");
+		//	// Act
+		//	controller.Put(5, "value");
 
-            // Assert
-        }
+		//	// Assert
+		//}
 
-        [Test]
-        public void Delete()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
+		//[Test]
+		//public void Delete()
+		//{
+		//	// Arrange
+		//	ValuesController controller = new ValuesController();
 
-            // Act
-            controller.Delete(5);
+		//	// Act
+		//	controller.Delete(5);
 
-            // Assert
-        }
+		//	// Assert
+		//}
     }
 }

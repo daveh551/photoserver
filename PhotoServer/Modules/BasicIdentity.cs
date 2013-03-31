803f51ad8f62b99e8a8f7776894b81aa498a6434
Dave Hanna <dave@infotouchsys.com>
2013-03-31 17:19:26 -0500

    Implement BasicAuthentication in HttpModule

---
+++ PhotoServer/Modules/BasicIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace PhotoServer.Modules
+{
+	public class BasicIdentity : IIdentity
+
+	{
+		public string AuthenticationType
+		{
+			get { return "Basic"; }
+		}
+
+		private bool _isAuthenticated;
+
+		public bool IsAuthenticated
+		{
+			get { return _isAuthenticated; }
+		}
+
+		private readonly string _name;
+
+		public string Name
+		{
+			get { return _name; }
+		}
+
+		public BasicIdentity(string name)
+		{
+			_name = name;
+			_isAuthenticated = true;
+		}
+	}
+}
---
+++ PhotoServer/Modules/HttpAuthenticationModule.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+using Elmah;
+using WebMatrix.WebData;
+
+namespace PhotoServer.Modules
+{
+	public class HttpAuthenticationModule : IHttpModule
+	{
+		
+		public void Dispose()
+		{
+
+		}
+
+		public void Init(HttpApplication context)
+		{
+			var provider = Membership.Provider;
+			if (!WebSecurity.Initialized)
+				WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", false );
+			
+			context.AuthenticateRequest += OnAuthenticateRequest;
+		}
+
+		private void OnAuthenticateRequest(object sender, EventArgs e)
+		{
+			var app = sender as HttpApplication;
+			var credentials = app.Context.Request.Headers["Authorization"];
+			if (string.IsNullOrEmpty(credentials)) return;
+			//var userPassword = System.Convert.FromBase64String(credentials);
+			//var userString = (new System.Text.UTF8Encoding()).GetString(userPassword);
+			var encodedPassword = AuthenticationHeaderValue.Parse(credentials).Parameter;
+			var userPassword = new System.Text.UTF8Encoding().GetString(System.Convert.FromBase64String(encodedPassword));
+			var passwordParts = userPassword.Split(':');
+			var userName = passwordParts[0];
+			var password = passwordParts[1];
+
+			if (!WebSecurity.Initialized)
+				throw new System.ApplicationException("WebSecurity database became unitialized");
+			if (Membership.Provider.ValidateUser(userName, password))
+			{
+				var identity = new BasicIdentity(userName);
+				var roles = Roles.Provider.GetRolesForUser(userName);
+				var principal = new GenericPrincipal(identity, roles);
+
+				app.Context.User = principal;
+				if (HttpContext.Current != null)
+					HttpContext.Current.User = principal;
+
+			}
+			
+
+		}
+	}
+}
